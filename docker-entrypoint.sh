d6ec4d7b147746c6ec854516acb6fc88a6e86624
SUBxZERO <bangboy892005@gmail.com>
2026-06-21 14:16:36 +0700

    remove migrate:fresh from docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,7 +15,7 @@ fi
 
 # Run migrations and seed data (Force is needed for production environments)
 # This will create tables in the Neon PostgreSQL database and populate initial data
-php artisan migrate:fresh --force --seed
+php artisan migrate --force --seed
 
 # Execute the main container command (start Apache)
 exec apache2-foreground
